47511d83c42455b12c9ad0f796bc0be9c987f3c9
iChaosWolf <bryan.pichardo.b@gmail.com>
2022-09-20 18:10:07 -0400

    fizzbuzz

---
+++ webdev-assignments/week2-9-19-9-21/fizzbuzz.js
@@ -0,0 +1,28 @@
+
+finalNumber=60
+
+
+
+for(i=1;i<=finalNumber;i++){
+
+
+    if(i%15==0){
+
+        console.log(i+" "+"FizzBuzz");
+    }else if(i%3==0){
+
+           console.log(i+" "+"Fizz");
+
+    }else
+    if(i%5==0){ console.log(i+" "+"Buzz");}
+
+
+
+}
+
+
+
+
+
+
+
